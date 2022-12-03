6479d4a2ff330551058cc0bdf15279e90b21f595
FranciscoDiazPaccot73 <fran.diazpaccot@gmail.com>
2022-12-03 20:12:01 -0300

    add header

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -1,3 +1,9 @@
+import dynamic from 'next/dynamic';
+
+const Header = dynamic(() => import('../components/Header'), {
+  suspense: true,
+})
+
 import { useEffect, useState } from 'react';
 
 import '../styles/globals.css'
@@ -12,24 +18,9 @@ function MyApp({ Component, pageProps }) {
     }
   }, [])
 
-  const handleBack = () => window.history.back()
-
   return (
     <div>
-      {showHeader ? (
-        <header className='h-10 p-2 max-w-3xl mx-auto'>
-          <div onClick={handleBack} className="cursor-pointer inline-block">
-            <div className='flex gap-2'>
-              <svg viewBox="0 0 320 512" fill='#fff' height={24} width={24}>
-                <path
-                  d="M34.52 239.03L228.87 44.69c9.37-9.37 24.57-9.37 33.94 0l22.67 22.67c9.36 9.36 9.37 24.52.04 33.9L131.49 256l154.02 154.75c9.34 9.38 9.32 24.54-.04 33.9l-22.67 22.67c-9.37 9.37-24.57 9.37-33.94 0L34.52 272.97c-9.37-9.37-9.37-24.57 0-33.94z"
-                />
-              </svg>
-              <span>Back</span>
-            </div>
-          </div>
-        </header>
-      ) : null}
+      {showHeader ? <Header /> : null}
       <Component {...pageProps} />
     </div>
   )
