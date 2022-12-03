ba147e18133577de127ba71c18be211a93b65754
FranciscoDiazPaccot73 <fran.diazpaccot@gmail.com>
2022-12-03 20:06:44 -0300

    add day 2 and 3 - Fran

--- pages/fran/day2.jsx
+++ pages/fran/day2.jsx
@@ -1,9 +1,9 @@
 const ELEMENTS = ['Medias', 'Caramelos', 'Vitel Tone']
 
 const Day2 = () => (
-  <div className="p-8 pt-0">
-    <div className="min-h-screen pb-16 flex-1 flex flex-col justify-center items-center">
-      <p className=" text-4xl mb-8">Regalos</p>
+  <div className="min-h-screen p-8 pt-0 bg-[url('/christmas.jpg')] bg-cover bg-no-repeat flex justify-center items-center">
+    <div className="p-32 rounded flex flex-col justify-center items-center bg-slate-900 w-40 h-48">
+      <p className=" text-4xl mb-8">Regalos:</p>
       <ul className="flex items-center justify-center flex-col">
         {ELEMENTS?.map(elem => (
           <li key={elem}>{elem}</li>
