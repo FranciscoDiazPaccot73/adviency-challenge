52c3b37cccf1b7ea81e943c0902a3f3c7f6987e5
joelwaiman <waimanjoel@gmail.com>
2022-12-16 13:05:30 -0300

    dia 8

---
+++ pages/joel/day16.jsx
@@ -0,0 +1,13 @@
+import Head from 'next/head';
+
+  const Day16 = () => (
+    <>
+      <Head>
+        <title>JOEL | Dia 16 | Adviency Challenge</title>
+        <meta name="description" content="Adviency Challenge" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+    </>
+  );
+  export default Day16;
+  
--- pages/joel/day8.jsx
+++ pages/joel/day8.jsx
@@ -1,13 +1,125 @@
-import Head from 'next/head';
+import { useState } from "react";
 
-  const Day8 = () => (
-    <>
-      <Head>
-        <title>JOEL | Adviency Challenge | Dia 8</title>
-        <meta name="description" content="Adviency Challenge" />
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-    </>
+const Day8 = () => {
+
+  const [presents, setPresents] = useState([])
+  const [inputValue, setInputValue] = useState('')
+  const [count, setCount] = useState(1)
+
+  const handleCatchValue = (e) => {
+    const { value } = e.target;
+    setInputValue(value)
+  }
+
+  const handleAddItem = () => {
+    if (presents.includes(inputValue)) {
+      console.log('Error');
+    } else {
+      const newPresent = {name:inputValue, count: count, id: inputValue}
+      const newPresents = [...presents, newPresent]
+      setPresents(newPresents, count)
+      setInputValue('')
+      setCount(1)
+    }
+  }
+
+  const deleteItem = (id)=>{
+    const itemToDelete = presents.filter(gift=>gift !== id)
+    setPresents(itemToDelete)
+  }
+
+  const clear = ()=>{
+    setPresents([])
+  }
+
+  const handleToAdd = () => {
+    setCount(count + 1)
+  }
+
+  const handleToSubtract = () => {
+    if (count > 1) {
+      setCount(count - 1)
+    }
+  }
+
+  return (
+    <div style={styles.body}>
+      <div style={styles.container}>
+        <p style={styles.title}>Regalos</p>
+        <div style={styles.inputGroup}>
+          <input value={inputValue} onChange={handleCatchValue} placeholder="Ingresa tu regalo" type="text" />
+          <button onClick={handleToSubtract}>-</button>
+          <span>{count}</span>
+          <button onClick={handleToAdd}>+</button>
+          <button style={styles.button} onClick={handleAddItem}>Agregar</button>
+        </div>
+        {presents.length === 0 ?
+          <p style={styles.empty}>Tu lista está vacia, agrega algun regalo :c</p>
+          : <ul style={styles.presents}>
+            {presents.map(gift => (
+              <li style={styles.itemList} key={gift.id}>
+                <span>{gift.name}</span>
+                <p>{gift.count}</p>
+                <button onClick={() => deleteItem(gift)}>X</button>
+              </li>
+            )
+            )}
+          </ul>}
+
+        <button onClick={clear} style={styles.button}>Borrar todo</button>
+      </div>
+    </div>
   );
-  export default Day8;
-  
+}
+export default Day8;
+
+const styles = {
+  body: {
+    backgroundImage: 'linear-gradient(to right, #E1415F 0%, #5B1A1A 100%)',
+    height: '100vh'
+  },
+  container: {
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
+    maxWidth: 850,
+    margin: 'auto',
+    paddingTop: 200,
+  },
+  inputGroup: {
+    display: 'flex',
+    justifyContent: 'space-between',
+    width: 275,
+    margin: '0 0 20px'
+  },
+  button: {
+    backgroundImage: 'linear-gradient(to right, #50D5B7 0%, #067D68 100%)',
+    alignItems: 'center',
+    padding: '5px 10px',
+    border: 'transparent',
+    borderRadius: 10,
+    boxShadow: '2px 2px 4px rgba(0,0,0,0.4)'
+  },
+  presents: {
+    display: 'flex',
+    flexDirection: 'column',
+    backgroundColor: '#3F4E4F',
+    borderRadius: 10,
+    width: 275,
+    margin: '0 0 10px',
+    padding: '5px 10px',
+  },
+  empty: {
+    marginBottom: 20
+  },
+  itemList: {
+    display: 'flex',
+    justifyContent: 'space-between',
+  },
+  title: {
+    fontFamily: 'Home Christmas',
+    color: '#ffde59',
+    margin: 25,
+    fontSize: '2.5rem'
+  }
+}
